49d498ba6c1404312082ee866981b71ffeb39ec5
Patrik Eriksson <126262829+pata02@users.noreply.github.com>
2025-09-26 16:19:40 +0200

    q1 completed

--- q1/shift_register_tb.sv
+++ q1/shift_register_tb.sv
@@ -1,5 +1,62 @@
 module shift_register_tb;
 
-// complete here
+    parameter N = 4;
+
+  // Testbench signals
+        logic clk;
+        logic rst_n;
+        logic serial_parallel;
+        logic load_enable;
+        logic serial_in;
+        logic [N-1:0] parallel_in;
+        logic [N-1:0] parallel_out;
+        logic serial_out;
+
+always #5 clk = ~clk;
+
+  // Instantiate the decoder module
+  shift_register uut (
+
+    .clk(clk),
+    .rst_n(rst_n),
+    .serial_parallel(serial_parallel),
+    .load_enable(load_enable),
+    .serial_in(serial_in),
+    .parallel_in(parallel_in),
+    .parallel_out(parallel_out),
+    .serial_out(serial_out)
+  );
+
+  // Complete your testbench here
+  initial begin
+    clk = 1;
+    rst_n = 0;
+    serial_in = 0;
+    parallel_in = 4'b1111;
+    serial_parallel = 1;
+    load_enable = 0;
+    rst_n = 1;
+    #10;
+
+    //Test parallell load without enable
+    load_enable = 0;
+    #10;
+
+    //Test parallell load with enable
+    load_enable = 1;
+    #10;
+
+    //Test serial load without enable
+    load_enable = 0;
+    serial_in = 0;
+    serial_parallel = 0;
+    #10;
+    //Test serial load with enable
+    load_enable = 1;
+    serial_parallel = 0;
+    serial_in = 0;
+    #60;
+    $finish;
+  end
 
 endmodule
--- q1/shift_regiter.sv
+++ q1/shift_regiter.sv
@@ -8,6 +8,20 @@ module shift_register #(parameter N=4)
                        output logic [N-1:0] parallel_out,
                        output logic serial_out);
 
-//complete here
+assign serial_out = parallel_out[0];
+
+always_ff @(posedge clk or negedge rst_n ) begin
+    if (!rst_n) begin
+        parallel_out <= '0;
+    end
+    else if (load_enable) begin
+        if (serial_parallel) begin
+            parallel_out <= parallel_in;
+        end
+        else begin
+            parallel_out <= {serial_in, parallel_out[N-1:1]};
+        end
+    end
+end
 
 endmodule
